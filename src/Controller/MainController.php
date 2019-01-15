628007187286f162d932b9cf70a4e3165de9749b
Ruslan Pomorov <r.pomorov@gmail.com>
2019-01-15 11:01:34 +0200

    Redirection from Main page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_main")
+     * @return RedirectResponse
+     */
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_login');
+    }
+}
